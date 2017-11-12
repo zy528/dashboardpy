a0b051acb9a06757a4dfbcec1035572e497dc962
zy528 <30794610+zy528@users.noreply.github.com>
2017-11-12 19:41:41 +0800

    33

---
+++ firstdashboard/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from datas import GetData
+# Create your views here.
+def index(request):
+    getdata = GetData()
+    result = getdata.getuser()
+    for i in range(len(result)):
+        print(result[i])
+
+    return render(request,'first/index.html')
