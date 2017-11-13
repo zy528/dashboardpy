9d6e6cb0060b9b2c0477eaf9e0389aa052684ef6
zy <379435696@qq.com>
2017-11-13 18:47:36 +0800

    1

--- dashboardpy/settings.py
+++ dashboardpy/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'dashboardpy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'zy',
+        'NAME': 'd_easyhin_stat_ret',
         'USER':'root',
         'PASSWORD':'welcome123',
         'HOST': '127.0.0.1',
